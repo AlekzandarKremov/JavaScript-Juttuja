8e102136f011abd0bf982df560a4c3d764ecd8de
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-03-09 13:41:57 +0200

    muutoksia

--- Eka JS Juttu/First_Script.js
+++ Eka JS Juttu/First_Script.js
@@ -2,12 +2,43 @@
 
 var name = "Alek";
 var testi = "This Is A Test";
-
 var age = "16";
-
 var var1 = "20 20 20 20";
 var var2 = 120.5;
 
 
 
-alert(name + testi + age +var1)
+alert("Testi");
+
+var test = document.getElementById ("test");
+
+var oldtext = test.innerHTML;
+
+test.innerHTML = "Moi Miten Menee";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Log komento");
+console.error("Error komento");
+console.warn("Warn komento");
