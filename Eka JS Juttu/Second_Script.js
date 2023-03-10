981f680a81992aa97fe4ed2c319458a6cbfa1040
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-03-10 09:24:50 +0200

    k

--- Eka JS Juttu/Second_Script.js
+++ Eka JS Juttu/Second_Script.js
@@ -1,4 +1,16 @@
+
+
 document.getElementById("hoo1").style.color = "#9147BB";
 
 document.getElementById("hoo2").style.color = "#1EB5D5";
 
+
+document.getElementById("hoo3").style.color = "gold";
+var hoo3 = document.getElementById ("hoo3");
+var oldtext = hoo3.innerHTML;
+hoo3.innerHTML = "<u><i>Moi Miten Menee</i></u>";
+
+
+console.log("Log komento");
+console.error("Error komento");
+console.warn("Warn komento");
