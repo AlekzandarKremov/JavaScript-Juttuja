1abb51a9773fae1e4a2318a1f443147bcf5a4950
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-03-10 10:56:32 +0200

    Update script.js

--- mini-projekti-1/script.js
+++ mini-projekti-1/script.js
@@ -1,1 +1,6 @@
-var testi = document.getElementById("hoo1") 
+var jotain = "How Is It Going?";
+var testi = document.getElementById("hoo1");
+var teksti = "Tämä on testi";
+
+var lause = "Moi miten menee"  + testi
+
