e31e5b5f0225a44072e61ff2059778d25a04d42c
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-03-10 13:33:37 +0200

    asf

--- string manipulations/string.js
+++ string manipulations/string.js
@@ -2,5 +2,11 @@
 var sentenceNode = document.getElementById("sentence");
 var sentence = sentenceNode.innerText;
 
+var upperSentence = sentence.toUpperCase();
+var lowerSentence = sentence.toLowerCase();
+var firstTenChars = sentence.substring(0, 11);
+
+var allWords = sentence.split(" ");
+var bracketWords = "(" + allWords.join(")(") + ")"
 
 console.log("Sentence:", sentence)
