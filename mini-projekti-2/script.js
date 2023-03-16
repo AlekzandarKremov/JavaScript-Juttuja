6a334d9aa9edf7e89a1b3cab29b11c92d0da4dc1
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-03-16 12:41:09 +0200

    fasafsfasasfa

--- mini-projekti-2/script.js
+++ mini-projekti-2/script.js
@@ -2,7 +2,7 @@
 let url = prompt("Please enter a URL:");
 
 if (!url.startsWith("http")) {
-  url = "https://" + url + ".com";
+  url = "http://" + url + ".com";
 }
 
 document.getElementById("link").innerHTML = "<a href='" + url + "'>" + url + "</a>";
