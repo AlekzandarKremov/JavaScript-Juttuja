dbbb181f4ccbb745977dc95754bb66b63cb35e83
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-03-16 09:14:51 +0200

    dggdaag

--- string manipulations/string.js
+++ string manipulations/string.js
@@ -9,4 +9,13 @@ var firstTenChars = sentence.substring(0, 11);
 var allWords = sentence.split(" ");
 var bracketWords = "(" + allWords.join(")(") + ")"
 
+var firstThreeWords = allWords.slice(0, 3);
+
+var middleWords = allWords.slice(5, 10);
+var makeSentence = middleWords.join(" ")
+
+sentenceNode.innerText = makeSentence
+
+
+
 console.log("Sentence:", sentence)
