7e696b704a4fedfc73bcf0e2fe4cbc691930016d
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-03-16 11:16:31 +0200

    huono koodi

--- operators/script2.js
+++ operators/script2.js
@@ -3,7 +3,7 @@ var inputLower = input.toLowerCase()
 
 var text = document.getElementById("text")
 
-if(inputLower == "Javascript for beginners") {
+if(inputLower == "javascript for beginners") {
 text.innerHTML = "<strong>WELCOME</strong> to Javascript for <strong>Beginners</strong>"
 } else {
     text.innerHTML = "You're in the wrong course";
