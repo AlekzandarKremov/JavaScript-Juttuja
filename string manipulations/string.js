e7ecfe18afddfcbbc6e09a0e2d99a8f4b65c6f40
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-03-16 09:24:12 +0200

    et

--- string manipulations/string.js
+++ string manipulations/string.js
@@ -4,15 +4,15 @@ var sentence = sentenceNode.innerText;
 
 var upperSentence = sentence.toUpperCase();
 var lowerSentence = sentence.toLowerCase();
-var firstTenChars = sentence.substring(0, 11);
+var firstElevenChars = sentence.substr(0, 11);
 
 var allWords = sentence.split(" ");
-var bracketWords = "(" + allWords.join(")(") + ")"
+var bracketWords = "(" + allWords.join("") + ")"
 
 var firstThreeWords = allWords.slice(0, 3);
 
 var middleWords = allWords.slice(5, 10);
-var makeSentence = middleWords.join(" ")
+var makeSentence = middleWords.join(" ");
 
 sentenceNode.innerText = makeSentence
 
