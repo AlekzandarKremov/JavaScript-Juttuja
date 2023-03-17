88b91c4aa814072259375106bf526bbd797dfab1
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-03-17 08:21:51 +0200

    projekti

---
+++ joku/script.js
@@ -0,0 +1,13 @@
+var familyMember = "Wade";
+
+switch(familyMember) {
+    case "Alek":
+        console.log("Hi Alek")
+        break;
+    case "Kissa":
+        console.log("Aww Cute Cat")
+        break;
+     default:
+        console.warn("Not handled")
+        break;
+}
