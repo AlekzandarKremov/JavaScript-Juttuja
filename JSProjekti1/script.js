a061645afc06754a2bacd37578df0bcb3d963d88
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-03-17 09:32:01 +0200

    asd

---
+++ JSProjekti1/script.js
@@ -0,0 +1,59 @@
+const months = [  
+ "January",
+ "February",
+ "March",
+ "April",
+ "May",
+ "June",
+ "July",
+ "August",
+ "September",
+ "October",
+ "November",
+ "December",
+];
+const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2023,2,20,8,30,0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()];
+
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${year} ${hours}:${minutes}am`;
+
+// future time in ms
+const futureTime = futureDate.getTime();
+
+function getRemainingTime(){
+const today = new Date().getTime();
+const t = futureTime - today;
+// 1s = 1000ms
+// 1m = 60s
+// 1hr = 60min
+// 1d = 24hr
+
+// values in ms
+const oneDay = 24*60*60*1000
+}
+
+getRemainingTime();
+
