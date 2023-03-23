00938c061891f230be7b578334bbe72c95680560
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-03-23 08:45:43 +0200

    ad

---
+++ Code_Comments/script.js
@@ -0,0 +1,11 @@
+// console.log("Comment #1")
+
+/*
+console.log("Comment #2")
+console.log("Comment #2")
+console.log("Comment #2")
+console.log("Comment #2")
+*/
+
+var name = "Alek"; // This is my Name
+console.log("Do I Show Up?")
---
+++ IfElse/script.js
@@ -0,0 +1,26 @@
+var familymember = "Wade";
+
+switch(familymember) {
+    case "Alek":
+        console.log("Hi Alek")
+        break;
+    case "Nathan":
+        break;
+    default:
+        console.warn("Not Handled")
+        break;
+}
+
+
+
+var name = "Alek";
+
+if(name == "Alek") {
+    console.log("AWESOME");
+} else if(name == "Zephyr") {
+    console.log("MEOW");
+} else if(name == "Henry") {
+    console.log("Little Meow")
+} else {
+    console.warn("Not Handled")
+}
