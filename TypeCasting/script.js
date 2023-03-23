d3263d0ded18f2f23b2e457924ab8e4d865850c6
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-03-23 13:40:26 +0200

    typecasting

---
+++ TypeCasting/script.js
@@ -0,0 +1,33 @@
+/*
+var age = "30";
+age = Number(age);
+console.log(typeof(age))
+*/
+
+/*
+var age = prompt("Your age is?")
+age = Number(age)
+if(age) {
+    alert("You Have A Proper Number")
+
+    if(age >= 18) {
+        console.log("You can vote!")
+    }
+}
+console.log(typeof(age))
+*/
+
+/*
+var age = 30;
+age = String(age)
+console.log(age, typeof(age))
+*/
+
+/*
+var isCat = "true"
+isCat = Boolean(isCat)
+console.log(isCat, typeof(isCat))
+*/
+
+var course = "JS For Beginners";
+course = Array(course)
