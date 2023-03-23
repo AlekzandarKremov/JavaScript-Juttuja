408ac26e6f171c973325c0a569fb27761274efad
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-03-23 09:25:39 +0200

    Update script.js

--- Boolean/script.js
+++ Boolean/script.js
@@ -1,5 +1,21 @@
-var isCat = true;
+var isCat = false;
 
 if(isCat) {
     console.log("ZEPHYR IS A CAT")
+} else {
+    console.log("IS NOT A CAT")
+}
+
+var name = prompt("Enter Name")
+
+if(name.toLowerCase() == "zephyr") {
+    var isCat = true;
+} else {
+    var isCat = false;
+}
+
+if(isCat) {
+    alert("You're a cat!")
+} else {
+    alert("You're NOT a cat")
 }
