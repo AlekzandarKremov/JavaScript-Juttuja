0f1741c0281a2c044380a6ad7d49bd23c2f6c20f
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-03-24 11:11:36 +0200

    asdasd

---
+++ Functions/script.js
@@ -0,0 +1,10 @@
+function greeting(name="Alekki", age=17) {
+    console.log("Hello " + name + "!")
+    console.log("You are " + age + " Years Old!")
+
+    return name;
+}
+
+var defaultName = greeting()
+var ElGato = greeting("El Gato", 204)
+var Kronkodrilo = greeting("Kronkodriilo", 9087)
