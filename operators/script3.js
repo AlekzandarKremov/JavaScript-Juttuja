c2c0e410a442a4137cef7318a5e0669b10f114b8
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-03-24 08:48:36 +0200

    Update script3.js

--- operators/script3.js
+++ operators/script3.js
@@ -4,8 +4,41 @@ if(course != "php") {
     console.log("Is a JS course!")
 }
 */
+/*
+var age = 18;
+if(age >= 18) {
+    console.log("You Can Vote!")
+} else if(age < 18) {
+    console.log("You Can't Vote")
+}
 
-var age = 17;
-if(age > 18) {
-    console.log("You Can Play Pro Soccer")
+if(age >= 18) {
+    console.log("You Can Drink Alcohol")
+} else if(age < 18) {
+    console.log("You Can't Drink Alcohol")
+}
+*/
+/*
+var fullName = "Alekki  Palekki";
+if(fullName.length >= 10) {
+    console.log("Long Name")
+} else if(fullName.length < 10) {
+    console.log("Short Name")
+}
+*/
+/*
+var name = "El Gato";
+if(name.lenght <= 10) {
+    console.log(
+        "Total Lenght is <= 10",
+        name.lenght
+    )
+}
+*/
+
+var age = "30";
+if(age === 30) {
+    console.log("You Are Thirty")
+} else {
+    console.warn("Wrong Data Type")
 }
