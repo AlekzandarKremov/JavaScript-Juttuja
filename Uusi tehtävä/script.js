aa83f2115dbb13fdf1fb4c36568de8fe5bd38c67
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-04-11 10:52:32 +0300

    asdasda

--- Uusi tehtävä/script.js
+++ Uusi tehtävä/script.js
@@ -1,13 +1,20 @@
-var name = prompt("What Is Your Name?", "Alek");
-name = name.toLowerCase();
-var h3Node = document.getElementById("customInput");
-h3Node.innerText = name;
+
+//Tähän olen tehnyt promptin joka kysyy käyttäjältä nimeä
+var name = prompt("What Is Your Name?");
+var name2 = prompt("What Is Your Name? part 2");
+var name3 = prompt("What Is Your Name? part 3");
+//Nämä nämä hakee erilaiset custominputit html tiedostosta
+var hoo3 = document.getElementById("customInput");
+var hoo32 = document.getElementById("customInput2");
+var hoo33 = document.getElementById("customInput3");
+//seuraavat 3 komentoa muokkaavat tekstiä
+//Tämä näyttää tekstin niin kuin se on kirjoitettu
+hoo3.innerText = name;
+//Tämä tekee kaikista tekstin kirjaimista isoja
+hoo32.innerText = name2.toUpperCase();
+//Tämä tekee kaikista tekstin kirjaimista pieniä
+hoo33.innerText = name3.toLowerCase();
 
 
-
-
-
-
-
-
+//Tässä on myös console log joka nöyttää käyttäjän vastauksen
 console.log("Your Name Is", name)
