0fd16d3b0055744544a7f6161cb88ed57433f784
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-04-11 10:13:47 +0300

    things

---
+++ QuerySelector/script.js
@@ -0,0 +1,11 @@
+//var title = document.querySelector(".title")
+//title.innerText = "Changed using a class"
+//var small = document.querySelector("small")
+//small.innerText = "WOW IM TINY TEXT"
+//var small = title.querySelector("small")
+//small.innerText = "WOWOWOWOWOW"
+//var a = document.querySelector("a")
+//a.innerText = "Coding For Everybody.com"
+//var lis = document.querySelectorAll("li")
+var input = document.querySelector(".form-control")
+console.log(input.value)
