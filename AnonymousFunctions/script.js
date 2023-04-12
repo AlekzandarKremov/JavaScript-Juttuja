029a034ad5d9a9107c6f74ba1795e274795d88a6
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-04-12 13:04:08 +0300

    as

---
+++ AnonymousFunctions/script.js
@@ -0,0 +1,9 @@
+/*var getName = function() {
+    return document.querySelector(".js-customName").value
+};*/
+
+function LoadApp() {
+    console.log("App Is Loading")
+    // .......
+    console.log("App Is Loaded")
+}
