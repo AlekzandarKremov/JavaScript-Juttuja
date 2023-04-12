d67d1a87cf58906139b8a1776e5b694a7216ee8a
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-04-12 09:04:25 +0300

    asd

---
+++ custominput/script.js
@@ -0,0 +1,13 @@
+var name = prompt("What Is Your Name?", "Alek");
+name = name.toLowerCase();
+var h3Node = document.getElementById("customInput");
+h3Node.innerText = name;
+
+
+
+
+
+
+
+
+console.log("Your Name Is", name)
