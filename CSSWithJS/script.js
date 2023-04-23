ebdb8d19026238020364b9167e64112ad9056fea
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-04-23 15:40:27 +0300

    Update script.js

--- CSSWithJS/script.js
+++ CSSWithJS/script.js
@@ -3,13 +3,12 @@ const bgInput = document.querySelector('#bg-color');
 const textInput = document.querySelector('#text-color');
 const submitBtn = document.querySelector('#submit-btn');
 
-// Asetetaan tapahtumankuuntelijat painikkeelle
 submitBtn.addEventListener('click', function() {
   // Haetaan syötteet
   const bgColor = bgInput.value;
   const textColor = textInput.value;
 
-  // Asetetaan taustaväri ja tekstin väri
+  // Vaihdetaan taustaväri ja tekstin väri
   document.body.style.backgroundColor = bgColor;
   document.body.style.color = textColor;
 });
