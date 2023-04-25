54c66f2514fc71a4271dc1a61a51bf237fb3dd54
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-04-25 13:43:18 +0300

    g

---
+++ ForLoops/script.js
@@ -0,0 +1,16 @@
+var names = ["Alekki", "ElGato", "Kokotriilo", "CupcakeThePitbull"]
+
+function toEmailAddress(name) {
+    return name.toLowerCase( + "@gmail.com")
+}
+
+
+for(var i = 0; i < names.length; i++) {
+    var currentName = names[i]
+    console.log(currentName)
+}
+
+
+/*for(var i=0; i < 10; i++) {
+    console.log(i)
+}*/
