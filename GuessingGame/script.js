93b12ad9d21acc2d8799fbd2ed8d3095bf730e26
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-04-27 09:28:12 +0300

    game

---
+++ GuessingGame/script.js
@@ -0,0 +1,13 @@
+
+var numero = 42
+
+while(true) {
+    var number = Number(prompt("What Is The Number"))
+
+if(number) {
+    if(number == numero) {
+        alert("You win")
+        break;
+    } 
+    }
+}
