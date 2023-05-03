d411483fff26be8afee3fd27e4521fcd8cd1cc96
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-05-03 08:44:16 +0300

    Create script2.js

---
+++ Literals/script2.js
@@ -0,0 +1,10 @@
+const computer = "computer"
+const person = {
+    "name" : "Alekki",
+    speak() {
+        console.log("I SAY WOOF")
+    },
+    computer,
+}
+console.log(person.computer)
+person.speak()
