f1accabc6bb9a32ba3aa0dd2af08742ce7238d02
Alek <alekzandarkremov@gmail.com>
2023-08-09 11:01:09 +0300

    game changes

--- MemoryGame/script.js
+++ MemoryGame/script.js
@@ -3,6 +3,17 @@ const numberOfSquares = 16;
 let i = 0;
 let square1, square2;
 let clickCount = 0;
+let score = 0;
+
+let bgMusic = new Audio("Hello.wav")
+bgMusic.play();
+bgMusic.loop = true;
+
+document.querySelector("#score").style.visibility = "hidden";
+const playAgainBtn = document.querySelector('button')
+playAgainBtn.style.visibility = "hidden"
+playAgainBtn.addEventListener('click', playAgain)
+
 
 let colors = [
     "#33ff33",
@@ -46,16 +57,60 @@ for(const square of squares) {
 }
 
 function squareClicked() {
+    if(square1 == this) return;
     clickCount++
+    if(clickCount > 2) return;
     clickCount === 1 ? (square1 = this) : (square2 = this);
     if(clickCount === 1) {
         square1.style.background = square1.getAttribute("data-color");
     } else {
         square2.style.background = square2.getAttribute("data-color");
+        checkMatch()
     }
 }
 
-//selectColor();
-//selectColor();
+function checkMatch() {
+    let match =
+    square1.getAttribute("data-color") === square2.getAttribute("data-color")
+    if(!match) {
+        setTimeout(function() {
+            noMatch();
+        }, 500);
+    } else {
+        isMatch()
+        checkGameEnded()
+    }
+}
 
-//console.log(selectColor());
+function noMatch() {
+    square1.style.background = ""
+    square2.style.background = ""
+    square1 = ""
+    square2 = ""
+    clickCount = 0
+    console.log("No Match")
+}
+function isMatch() {
+    score++
+    document.querySelector("#score").innerText = score;
+    document.querySelector("#score").style.visibility = "visible";
+    square1.style.border = "none"
+    square2.style.border = "none"
+    square1.removeEventListener("click", squareClicked);
+    square2.removeEventListener("click", squareClicked);
+    clickCount = 0
+    console.log("Is A Match")
+}
+
+
+function checkGameEnded() {
+    const target = numberOfSquares / 2;
+    const gameOver = score === target ? true : false;
+    if(gameOver) {
+        playAgainBtn.style.visibility = "visible"
+    }
+}
+
+function playAgain() {
+    window.location.reload()
+}
